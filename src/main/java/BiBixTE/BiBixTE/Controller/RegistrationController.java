f4688d5b7b390e0949866d8137ef17b61aae872a
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-13 17:37:53 +0200

    preparing to heroku

---
+++ src/main/java/BiBixTE/BiBixTE/Controller/RegistrationController.java
@@ -0,0 +1,8 @@
+package BiBixTE.BiBixTE.Controller;
+
+public class RegistrationController {
+    public String registration(){
+        System.out.println("Hello world!");
+        return "index.html";
+    }
+}
--- src/main/java/BiBixTE/BiBixTE/Repository/CorrieriRepository.java
+++ src/main/java/BiBixTE/BiBixTE/Repository/CorrieriRepository.java
@@ -4,5 +4,4 @@ import BiBixTE.BiBixTE.Entity.Bibite;
 import BiBixTE.BiBixTE.Entity.Corrieri;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CorrieriRepository extends JpaRepository<Corrieri, Long> {
-}
+public interface CorrieriRepository extends JpaRepository<Corrieri, Long> {}
