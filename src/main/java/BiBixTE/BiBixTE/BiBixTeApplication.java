294b257cb5b215cfa955f1baa8d8078de502edc4
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-13 15:00:39 +0200

    Initial commit

---
+++ src/main/java/BiBixTE/BiBixTE/BiBixTeApplication.java
@@ -0,0 +1,13 @@
+package BiBixTE.BiBixTE;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiBixTeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BiBixTeApplication.class, args);
+	}
+}
---
+++ src/test/java/BiBixTE/BiBixTE/BiBixTeApplicationTests.java
@@ -0,0 +1,13 @@
+package BiBixTE.BiBixTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiBixTeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
