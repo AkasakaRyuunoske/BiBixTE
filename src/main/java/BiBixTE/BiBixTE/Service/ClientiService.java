c0d31606caf287b98d65576d9cadd869b41220f1
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-20 19:22:02 +0200

    Activated user func added

--- src/main/java/BiBixTE/BiBixTE/Service/ClientiService.java
+++ src/main/java/BiBixTE/BiBixTE/Service/ClientiService.java
@@ -1,9 +1,11 @@
 package BiBixTE.BiBixTE.Service;
 
 import BiBixTE.BiBixTE.Entity.Clienti;
+import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 
+@Service
 public interface ClientiService {
     void registerClienti(Clienti clienti) throws MessagingException;
 
