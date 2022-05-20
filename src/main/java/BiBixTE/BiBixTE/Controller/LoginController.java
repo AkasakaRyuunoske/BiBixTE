ee3a680d6372c22de2777fdf6d7ae0f7acd68d0a
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-20 19:23:33 +0200

    Authentication configured

--- src/main/java/BiBixTE/BiBixTE/Controller/LoginController.java
+++ src/main/java/BiBixTE/BiBixTE/Controller/LoginController.java
@@ -1,18 +1,24 @@
 package BiBixTE.BiBixTE.Controller;
 
 import BiBixTE.BiBixTE.Entity.Clienti;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+@Slf4j
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model){
-        model.addAttribute("cliente", new Clienti());
-        System.out.println("login");
+    public String login(@ModelAttribute Clienti clienti, Model model){
+        model.addAttribute("cliente", clienti);
+        if (clienti != null){
+            log.info("Clienti getUserName: " + clienti.getUserName());
+            log.info("Clienti getPassword: " + clienti.getPassword());
+            log.info("Clienti getEmail: " + clienti.getEmail());
+        }
         return "login";
     }
 }
