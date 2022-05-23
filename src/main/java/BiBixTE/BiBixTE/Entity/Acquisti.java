b60bf5be825731eb8e5873f5849988e69cf1ff73
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-23 20:57:24 +0200

    ready to deploy

--- src/main/java/BiBixTE/BiBixTE/Entity/Acquisti.java
+++ src/main/java/BiBixTE/BiBixTE/Entity/Acquisti.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.sql.Date;
-import java.util.List;
 
+@Slf4j
 @Entity
 @Data
 @AllArgsConstructor
@@ -31,18 +31,20 @@ public class Acquisti implements Serializable {
     private int stock;
 
     @Column(name = "data_aquisto", nullable = false)
-    private Date data_aquisto;
+    private String data_aquisto;
 
     @Column(name = "importo", nullable = false)
     private Double importo;
 
-//    @OneToMany(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "id_cliente", referencedColumnName = "id_cliente_fk")
-//    List<Clienti> clienti;
+    @Column(name = "descrizione", nullable = false)
+    private String descrizione;
 
-//    @OneToMany(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "id_bibita", referencedColumnName = "id_bibita_fk")
-//    List<Bibite> bibite;
+//    @Column(name = "id_cliente_fk", nullable = true)
+//    private Long id_cliente_fk;
+//
+//    @Column(name = "id_bibita_fk", nullable = true)
+//    private Long id_bibita_fk;
+
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_cliente_fk", referencedColumnName = "id_cliente")
     private Clienti clienti;
@@ -50,4 +52,28 @@ public class Acquisti implements Serializable {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_bibita_fk", referencedColumnName = "id_bibita")
     private Bibite bibite;
+
+    public Acquisti(String data_aquisto, double importo, int quantita) {
+        this.data_aquisto = data_aquisto;
+        this.importo = importo;
+        this.stock = quantita;
+    }
+
+    public Acquisti(String data_aquisto, double importo, int quantita, String descrizione) {
+        this.data_aquisto = data_aquisto;
+        this.importo = importo;
+        this.stock = quantita;
+        this.descrizione = descrizione;
+    }
+
+    public Acquisti(String data_aquisto, double importo, int quantita, String descrizione, Long id_bibita, Long id_cliente) {
+        this.data_aquisto = data_aquisto;
+        this.importo = importo;
+        this.stock = quantita;
+        this.descrizione = descrizione;
+//        this.id_cliente_fk = id_cliente;
+//        this.id_bibita_fk = id_bibita;
+
+        log.info("Acquisto created!");
+    }
 }
