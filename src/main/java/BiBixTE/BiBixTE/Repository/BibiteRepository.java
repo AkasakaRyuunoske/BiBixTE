02eef5e0a552c615ad6742b52465579069c8b05d
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-23 20:57:39 +0200

    ready to deploy

--- src/main/java/BiBixTE/BiBixTE/Repository/BibiteRepository.java
+++ src/main/java/BiBixTE/BiBixTE/Repository/BibiteRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BibiteRepository extends JpaRepository<Bibite, Long> {}
+public interface BibiteRepository extends JpaRepository<Bibite, Long> {
+    Bibite findByMarca(String marca);
+}
