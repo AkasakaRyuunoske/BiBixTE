711f184201430c7eef643ef8891fc03e9d03fad0
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-23 16:46:26 +0200

    ready to deploy

--- src/main/java/BiBixTE/BiBixTE/Controller/AcquistiController.java
+++ src/main/java/BiBixTE/BiBixTE/Controller/AcquistiController.java
@@ -1,15 +1,22 @@
 package BiBixTE.BiBixTE.Controller;
 
+import BiBixTE.BiBixTE.Repository.AcquistiRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
 public class AcquistiController {
-    @GetMapping("/acquisti")
-    public String aquistiGET(){
+    @Autowired
+    AcquistiRepository acquistiRepository;
+
+    @GetMapping("/acquista/monster")
+    public String aquistiGET(Model model){
+        log.info("Prodotto: monster");
         return "acquisti";
     }
 
