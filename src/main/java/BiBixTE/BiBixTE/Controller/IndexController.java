4d3ebe6d6da1a3f472455fbc00c457f1d61e80a3
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-25 18:46:00 +0200

    Preparation for last deploy

--- src/main/java/BiBixTE/BiBixTE/Controller/IndexController.java
+++ src/main/java/BiBixTE/BiBixTE/Controller/IndexController.java
@@ -7,15 +7,12 @@ import BiBixTE.BiBixTE.Service.CustomUserDetails;
 import BiBixTE.BiBixTE.Service.MailSenderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @Slf4j
 @Controller
@@ -45,32 +42,9 @@ public class IndexController {
 
     @PostMapping("/")
     public String indexPost(@ModelAttribute Clienti formData, Model model){
-//        try {
-//            Clienti client = clientiRepository.findByUserName(formData.getUserName());
-//            if (!passwordEncoder.matches(formData.getPassword(), client.getPassword())){
-//                log.info("does not matches");
-//                return "clienteGiaEssiste";
-//            }
-//            model.addAttribute("clienti", client);
-//
-//            log.info("=====================================");
-//            log.info("User name:" + client.getUserName());
-//            log.info("Password:" + client.getPassword());
-//            log.info("Password from data:" + formData.getPassword());
-//            log.info("Email:" + client.getEmail());
-//            log.info("=====================================");
-//        } catch (NullPointerException nullPointerException){
-//            return "clienteGiaEssiste";
-//        }
         log.info("Form data: " + formData.getUserName());
         clientiDetailsService.loadUserByUsername(formData.getUserName());
         return "index.html";
-    }
-
-
-    @GetMapping("/greeting")
-    public ResponseEntity<String> greeting(@RequestHeader("accept-language") String language) {
-        return new ResponseEntity<String>("greeting", HttpStatus.OK);
     }
 
     @GetMapping("/crediti")
