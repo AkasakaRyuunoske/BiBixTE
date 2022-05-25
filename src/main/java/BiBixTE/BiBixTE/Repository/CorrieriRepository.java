e5f7bc481626319265bcdd9fd42f9d02b12db6d7
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-25 19:55:03 +0200

    Preparation for last deploy

--- src/main/java/BiBixTE/BiBixTE/Repository/CorrieriRepository.java
+++ src/main/java/BiBixTE/BiBixTE/Repository/CorrieriRepository.java
@@ -1,9 +1,10 @@
 package BiBixTE.BiBixTE.Repository;
 
-import BiBixTE.BiBixTE.Entity.Bibite;
 import BiBixTE.BiBixTE.Entity.Corrieri;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CorrieriRepository extends JpaRepository<Corrieri, Long> {}
+public interface CorrieriRepository extends JpaRepository<Corrieri, Long> {
+    Corrieri findByNome(String nome);
+}
