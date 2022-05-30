c26d59acd6844223d50e2fbc101de5abd9af8aa7
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-30 19:28:04 +0200

    Service logic deleted from controller

--- src/main/java/BiBixTE/BiBixTE/Controller/AcquistiController.java
+++ src/main/java/BiBixTE/BiBixTE/Controller/AcquistiController.java
@@ -1,11 +1,10 @@
 package BiBixTE.BiBixTE.Controller;
 
-import BiBixTE.BiBixTE.Entity.Acquisti;
-import BiBixTE.BiBixTE.Entity.Bibite;
 import BiBixTE.BiBixTE.Entity.Clienti;
 import BiBixTE.BiBixTE.Repository.AcquistiRepository;
 import BiBixTE.BiBixTE.Repository.BibiteRepository;
 import BiBixTE.BiBixTE.Repository.ClientiRepository;
+import BiBixTE.BiBixTE.Service.AcquistiServiceImp;
 import BiBixTE.BiBixTE.Service.ClientiServiceImp;
 import BiBixTE.BiBixTE.Service.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +14,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Controller
@@ -33,6 +29,8 @@ public class AcquistiController {
 
     @Autowired
     ClientiServiceImp clientiServiceImp;
+    @Autowired
+    AcquistiServiceImp acquistiServiceImp;
 
     @GetMapping("/acquista/{nome_bibita}/{importo}/{quantita}")
     public String aquistiBibitaGET(Model model,
@@ -46,88 +44,10 @@ public class AcquistiController {
 
         Double conto = cliente.getConto();
 
-        Acquisti acquisti;
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String data_acquisto = dateTimeFormatter.format(LocalDateTime.now());
-
-        if (quantita == 0){
-            log.info("No pepsi? :(");
-            return "err-product";
-        }
-
-        Bibite bibita = bibiteRepository.findByMarca(nome_bibita);
-        String acquisti_descrizione = "Prodotti acquistati: " + bibita.getMarca();
-        String background = "/" + bibita.getImagine();
-
-        model.addAttribute("marca", bibita.getMarca());
-        model.addAttribute("descrizione", bibita.getDescrizione());
-        model.addAttribute("capacita", bibita.getCapacita());
-        model.addAttribute("quantita",quantita);
-        model.addAttribute("importo", importo);
-        model.addAttribute("userName", userName);
-        model.addAttribute("conto", conto_to_display);
-
-        model.addAttribute("background_image", background);
-
-        try {
-            if (conto > importo) {
-                acquisti = new Acquisti(
-                        data_acquisto,
-                        importo,
-                        quantita,
-                        acquisti_descrizione,
-                        bibita,
-                        cliente
-                        );
-
-                clientiServiceImp.decreaseConto(importo);
-
-                int starting_quantita = bibita.getQuantita();
-                bibita.setQuantita(starting_quantita - quantita);
-
-                bibiteRepository.save(bibita);
-                acquistiRepository.save(acquisti);
-
-                log.info("Cliente" + cliente);
-                log.info("bibita" + acquisti);
-                log.info("importo" + importo);
-                log.info("acquisti" + acquisti);
-
-            } else {
-                return "err-balance";
-            }
-
-            if (!cliente.getEmail().isEmpty()) {
-                clientiServiceImp.sendConfirmAcquistoMail(cliente, bibita, acquisti, importo, quantita, data_acquisto);
-
-            } else {
-                return "err-general";
-            }
-
-        } catch (Exception exception) {
-            return "err-general";
-        }
-
-        log.info("=====================================");
-
-        log.info("Prodotto: " + nome_bibita);
-        log.info("time: " + data_acquisto);
-        log.info("importo: " + importo);
-        log.info("quantita: " + quantita);
-        log.info("userName: " + userName);
-
-        log.info("=====================================");
-
-        log.info("bibita getDescrizione: " + bibita.getDescrizione());
-        log.info("bibita getMarca: " + bibita.getMarca());
-        log.info("bibita getQuantita: " + bibita.getQuantita());
-        log.info("bibita getCapacita: " + bibita.getCapacita());
-        log.info("bibita getId_bibita: " + bibita.getId_bibita());
-
-        log.info("=====================================");
-
-        return "confirm";
+        return acquistiServiceImp.processAcquisto(quantita,
+                conto, importo,
+                model, nome_bibita,
+                cliente, conto_to_display);
     }
 
     @PostMapping("/acquisti")
