f2c20ce2a1cf68b90963e366f0db0c8854989b29
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-30 21:12:47 +0200

    Documentation added

--- src/main/java/BiBixTE/BiBixTE/Repository/ConsegneRepository.java
+++ src/main/java/BiBixTE/BiBixTE/Repository/ConsegneRepository.java
@@ -1,6 +1,5 @@
 package BiBixTE.BiBixTE.Repository;
 
-import BiBixTE.BiBixTE.Entity.Bibite;
 import BiBixTE.BiBixTE.Entity.Consegne;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
