caa61e02665cc543b61ecf5edcac7e472b31ce3f
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-05-30 21:11:32 +0200

    Documentation added

--- src/main/java/BiBixTE/BiBixTE/Service/AcquistiService.java
+++ src/main/java/BiBixTE/BiBixTE/Service/AcquistiService.java
@@ -6,6 +6,16 @@ import org.springframework.ui.Model;
 
 @Service
 public interface AcquistiService {
+
+    /**
+     * Will be used in @AcquistiServiceImp to process Acquisto(means Purchase)
+     * receiving these parameters from @AcquistiController.
+     *
+     * This method is even receiving Model model thus it adds attributes
+     * to the model returned by @AcquistoController
+     *
+     * */
+
     String processAcquisto(int quantita, Double conto,
                          Double importo, Model model,
                          String nome_bibita, Clienti cliente,
