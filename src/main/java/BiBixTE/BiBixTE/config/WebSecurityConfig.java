856a08c73be4e3deb71be71811896d7104e0831c
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-06-02 22:21:27 +0200

    Sessions working well

--- src/main/java/BiBixTE/BiBixTE/config/WebSecurityConfig.java
+++ src/main/java/BiBixTE/BiBixTE/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package BiBixTE.BiBixTE.config;
 
+import BiBixTE.BiBixTE.Service.ClientiDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -7,23 +8,16 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @EnableWebSecurity
+@EnableJdbcHttpSession
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-        // temporally is not needed, previous use was in the SecurityFilterChain
-        // if SecurityFilterChain will be needed this is needed too
-//    private static final String[] WHITE_LIST_URLS = {
-//            "/registration",
-//            "/registration-complete",
-//            "/",
-//            "/login",
-//            "/greeting",
-//            "/activate/*"
-//    };
+    @Autowired
+    private ClientiDetailsService clientiDetailsService;
 
     private static final  String[] INACCESSIBLE_URLS_BY_NOT_AUTHENTICATED_USERS = {
             "/",
@@ -42,27 +36,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(11);
     }
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity
-//                .cors()
-//                .and()
-//                .csrf()
-//                .disable()
-//                .authorizeHttpRequests()
-//                .antMatchers(WHITE_LIST_URLS)
-//                .permitAll();
-//
-//        return httpSecurity.build();
-//    }
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
     @Bean
     AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
+        provider.setUserDetailsService(clientiDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
 
         return provider;
@@ -70,35 +47,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf()
+        httpSecurity
+                .csrf()
                 .disable()
                 .authorizeRequests()
-//                .antMatchers("/")
-//                .permitAll()
-//                .antMatchers("/registration")
-//                .permitAll()
-//                .antMatchers("/registration-complete")
-//                .permitAll()
-//                .antMatchers("/activate/*")
-//                .permitAll()
-//                .antMatchers("/admin")
-//                .hasAuthority("AKASAKA")
                 .antMatchers(INACCESSIBLE_URLS_BY_NOT_AUTHENTICATED_USERS)
                 .authenticated()
                 .and()
                 .formLogin()
-
-                //to define custom login page, for some reason does not work
-                //may be needed in the future
-
-//                .loginPage("/login")
-//                .loginProcessingUrl("/login")
-//todo                .loginProcessingUrl("/j_spring_security_check")
                 .defaultSuccessUrl("/", true)
                 .permitAll()
                 .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/");;
+                .logoutSuccessUrl("/login")
+                .and()
+                .sessionManagement()
+                .maximumSessions(1);
     }
 }
