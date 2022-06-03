ccf1a97f2a793a4aeff7067be9d9bf1b85a0848b
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-06-03 14:50:40 +0200

    Sessions working well but still not perfect

--- src/main/java/BiBixTE/BiBixTE/Controller/IndexController.java
+++ src/main/java/BiBixTE/BiBixTE/Controller/IndexController.java
@@ -11,8 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,43 +26,64 @@ public class IndexController {
     @Autowired
     ClientiDetailsService clientiDetailsService;
 
+    /**
+     *
+     *  Here is common entry point for every user so here
+     *  unique session is set to a user following this steps:
+     *
+     *  1. Identify user by username and find him on repository
+     *     then set found client to a local object.
+     *
+     *  2. Get current session and PRIMARY_ID of the same session.
+     *
+     *  3. If client does not have session, this means he's just
+     *     redirected from login page, so sessionID is set and from
+     *     now on user is identified by his unique sessionID.
+     *
+     *     Data (Username, Conto) is set accordingly.
+     *
+     *     If a client already has sessionID so he is identified by that.
+     *
+     *  First part is unique for Index page controller.
+     *
+     *
+     * */
+
     @GetMapping("/")
     public String index(Model model, HttpServletRequest request){
 
+
         String userName = CustomUserDetails.clienti.getUserName();
-        String conto = "Your count: " + clientiRepository.findByUserName(userName).getConto().toString() + "€";
+
         Clienti clienti = clientiRepository.findByUserName(userName);
 
         String currentSession = request.getSession().getId();
+        final String SESSION_PRIMARY_ID = clientiRepository.getSessionPrimaryIDBySessionID(currentSession);
+
+
+        String conto;
+        String user_name_to_display;
 
         if (clienti.getSessionID() == null){
 
-            log.info("currentSession : " + currentSession);
-            log.info("clientiRepository.getSessionPrimaryIDBySessionID(currentSession)" + clientiRepository.getSessionPrimaryIDBySessionID(currentSession));
-            clienti.setSessionID(clientiRepository.getSessionPrimaryIDBySessionID(currentSession));
+            clienti.setSessionID(SESSION_PRIMARY_ID);
 
             clientiRepository.save(clienti);
-        }
 
-        log.info("userName: " + userName);
-        log.info("conto: " + conto);
-        log.info("Session getSession" + request.getSession().getId());
-        String  sessionID = clientiRepository.getSessionPrimaryIDBySessionID(currentSession);
-        clienti = clientiRepository.findBySessionID(sessionID);
-        String user_name_to_display = clienti.getUserName();
+            clienti = clientiRepository.findBySessionID(SESSION_PRIMARY_ID);
+
+            conto = "Your count: " + clienti.getConto().toString() + "€";
+            user_name_to_display = clienti.getUserName();
+
+        } else {
+            clienti = clientiRepository.findBySessionID(SESSION_PRIMARY_ID);
+
+            user_name_to_display = clienti.getUserName();
+            conto = "Your count: " + clienti.getConto().toString() + "€";
+        }
 
         model.addAttribute("userName", user_name_to_display);
         model.addAttribute("conto", conto);
-
-
-        return "index.html";
-    }
-
-    @PostMapping("/")
-    public String indexPost(@ModelAttribute Clienti formData, Model model){
-        log.info("Form data: " + formData.getUserName());
-
-        clientiDetailsService.loadUserByUsername(formData.getUserName());
 
         return "index.html";
     }
@@ -72,13 +91,16 @@ public class IndexController {
     @GetMapping("/crediti")
     public String crediti(Model model, HttpServletRequest request){
 
-        String userName = CustomUserDetails.clienti.getUserName();
-        String conto = "Your count: " + clientiRepository.findByUserName(userName).getConto().toString() + "€";
+        String currentSession = request.getSession().getId();
+        final String SESSION_PRIMARY_ID = clientiRepository.getSessionPrimaryIDBySessionID(currentSession);
 
-        model.addAttribute("userName", userName);
+        Clienti clienti = clientiRepository.findBySessionID(SESSION_PRIMARY_ID);
+
+        String user_name_to_display = clienti.getUserName();
+        String conto = "Your count: " + clienti.getConto().toString() + "€";
+
+        model.addAttribute("userName", user_name_to_display);
         model.addAttribute("conto", conto);
-
-        log.info("Session getSession" + request.getSession().getId());
 
         return "crediti.html";
     }
